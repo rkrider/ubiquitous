c8277e6e55015a2cbf568c37b5585d98efd1c381
Bumblebee <rk@Bumblebee.local>
2024-02-02 16:15:58 +0530

    first commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,9 @@
 function initMap() {
       // setting the specific location
-      var locationBangalore = {lat: 12.9805, lng: 77.5815};
-      var locationChennai = {lat: 13.080040010201738, lng: 80.26880345906798};
+      var locationBangalore = {lat: 13.1406270, lng: 77.5711820};
       // The map, centered at specified location
       var map = new google.maps.Map(
           document.getElementById('map1'), {zoom: 12, center: locationBangalore});
-        var map = new google.maps.Map(
-            document.getElementById('map2'), {zoom: 12, center: locationChennai});
       // The marker, positioned at specified location
       var marker = new google.maps.Marker({position: location, map: map});
     }
